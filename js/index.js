257b58fdf6561e33a33b7f7c072f480b1e1e0333
VikaLee <liweijia@91lds.com>
2016-10-18 22:10:23 +0800

    [CODE]:commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 $(function() {
 	//加载地图，调用浏览器定位服务
 	var map = new AMap.Map('container', {
-		resizeEnable: true
+		resizeEnable: true,
+		zoom:7
 	});
 	map.plugin(["AMap.ToolBar"], function() {
 		map.addControl(new AMap.ToolBar());
@@ -35,17 +36,105 @@ $(function() {
 
 	//解析定位错误信息
 	function onError(data) {
-		//初始化地图对象，加载地图
+		/*//初始化地图对象，加载地图
 		////初始化加载地图时，若center及level属性缺省，地图默认显示用户当前城市范围
 		var map = new AMap.Map('mapContainer', {
 			resizeEnable: true
-		});
+		});*/
 		document.getElementById('tip').innerHTML = '请开启手机定位信息';
 	}
 	
-	//图标偏移
-	$('.amap-geolocation-con').css('right', 'auto');
+	//定位图标偏移
+	/*$('.amap-geolocation-con').css('right', 'auto');
 	$('.amap-geolocation-con').css('bottom', '1.5625rem');
-	$('.amap-geolocation-con').css('left', '0.9375rem');
+	$('.amap-geolocation-con').css('left', '0.9375rem');*/
+	
+	/*map.setZoom(7);
+	console.log(map.getZoom());*/
+	//地图上Marker标记
+	var bikes = [{
+		"name": "尚东.数字山谷A区",
+		"center": "116.28782,40.042944",
+		"type": 1,
+		"subDistricts": []
+	}
+	, {
+		"name": "烈日影视大厦",
+		"center": "116.288796,40.04315",
+		"type": 1,
+		"subDistricts": []
+	}, {
+		"name": "天天易家",
+		"center": "116.288979,40.043757",
+		"type": 1,
+		"subDistricts": []
+	}, {
+		"name": "极加餐饮",
+		"center": "116.287943,40.043495",
+		"type": 1,
+		"subDistricts": []
+	}, {
+		"name": "网龙网络公司",
+		"center": "116.288372,40.043548",
+		"type": 1,
+		"subDistricts": []
+	}, {
+		"name": "车1",
+		"center": "116.288077,40.043248",
+		"type": 1,
+		"subDistricts": []
+	}, {
+		"name": "车2",
+		"center": "116.288233,40.043302",
+		"type": 1,
+		"subDistricts": []
+	}, {
+		"name": "车3",
+		"center": "116.288169,40.043047",
+		"type": 1,
+		"subDistricts": []
+	}, {
+		"name": "车4",
+		"center": "116.288576,40.04285",
+		"type": 1,
+		"subDistricts": []
+	}, {
+		"name": "车5",
+		"center": "116.288099,40.042686",
+		"type": 1,
+		"subDistricts": []
+	}, {
+		"name": "黄石科技大厦",
+		"center": "116.28885,40.044386",
+		"type": 1,
+		"subDistricts": []
+	}, {
+		"name": "东馨园",
+		"center": "116.289306,40.041622",
+		"type": 1,
+		"subDistricts": []
+	}];
+	getBikes();
+	$('.index_refresh').click(getBikes);
+	map.setFitView();
+	
+	function getBikes(){
+		var markers=[];
+		for (var i = 0; i < bikes.length; i += 1) {
+			var icon = new AMap.Icon({
+	            image : 'images/index_bike.png',
+	            //icon可缺省，缺省时为默认的蓝色水滴图标，
+	            size : new AMap.Size(35,39.5)
+	    	});
+			var marker = new AMap.Marker({
+				icon:icon,//24px*24px
+				position: bikes[i].center.split(','),
+				offset : new AMap.Pixel(-17.5,-19.75),
+				title: bikes[i].name,
+				map: map
+			});
+			markers.push(marker);
+		}		
+	}
 	
 });
