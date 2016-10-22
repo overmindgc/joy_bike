cf0a69416f67aa6281573964234dd0ca7ae34745
sunhaibin <ktufgeep@126.com>
2016-10-22 11:56:17 +0800

    js

---
+++ js/common.js
@@ -0,0 +1,53 @@
+/*
+ *提炼一些公用方法
+ * */
+
+//是否微信浏览器
+function isWeixin() {
+    var nav = navigator.userAgent;
+    if (/MicroMessenger/.test(nav)) {
+        return true;
+    } else {
+        return false;
+    }
+};
+function popup(s) {
+    if ($(".ev_popup").length == 0) {
+
+        $("body").append(
+        	'<div class="ev_popup" style="position:fixed;width:80%;top:48%;left:10%;text-align:center;line-height:38px;padding:10px;margin:0 auto">'+
+        	'<div style="margin:0 auto;background:#2a2a2a;color:#fff;border-radius:10px;">'+s+'</div></div>'
+        	);
+
+      
+    } else {
+        
+        $(".ev_popup div").text(s);
+    }
+    setTimeout(function() {
+        $(".ev_popup").remove();
+    }, 3000)
+}
+String.prototype.cutString = function(len) {
+    //length属性读出来的汉字长度为1
+    if (this.length * 2 <= len) {
+        return this;
+    }
+    var strlen = 0;
+    var s = "";
+    for (var i = 0; i < this.length; i++) {
+        s = s + this.charAt(i);
+        if (this.charCodeAt(i) > 128) {
+            strlen = strlen + 2;
+            if (strlen >= len) {
+                return s.substring(0, s.length - 1) + "...";
+            }
+        } else {
+            strlen = strlen + 1;
+            if (strlen >= len) {
+                return s.substring(0, s.length - 2) + "...";
+            }
+        }
+    }
+    return s;
+};
---
+++ js/search.js
@@ -0,0 +1,18 @@
+$(function(){
+	var ctrl={
+		path:{
+			history:"",
+			delete:""
+		},
+		init:function(){
+			
+		},
+		getHistory:function(){
+			
+		},
+		deleteHistory:function(){
+			
+		}
+	};
+	ctrl.init();
+})
